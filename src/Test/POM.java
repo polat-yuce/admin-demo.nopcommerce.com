160c5782663e77d63f9e630b5f08c0bd76081d9c
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2025-05-29 19:02:19 +0300

    us2

--- src/Test/POM.java
+++ src/Test/POM.java
@@ -17,4 +17,59 @@ public class POM {
 
     @FindBy(xpath = "//*[text()='Logout']")
     public WebElement logOut;
+
+    @FindBy(xpath = "(//ul[@role='menu']/li)[3]")
+    public WebElement catalog;
+
+    @FindBy(xpath = "(//ul[@role='menu']/li)[4]")
+    public WebElement sales;
+
+    @FindBy(xpath = "(//ul[@role='menu']/li)[5]")
+    public WebElement customers;
+
+    @FindBy(xpath = "(//ul[@role='menu']/li)[6]")
+    public WebElement promotions;
+
+    @FindBy(xpath = "(//ul[@role='menu']/li)[7]")
+    public WebElement contentManagement;
+
+    @FindBy(xpath = "(//ul[@role='menu']/li)[8]")
+    public WebElement configuration;
+
+    @FindBy(xpath = "(//ul[@role='menu']/li)[9]")
+    public WebElement system;
+
+    @FindBy(xpath = "(//ul[@role='menu']/li)[10]")
+    public WebElement reports;
+
+    @FindBy(xpath = "(//ul[@role='menu']/li)[11]")
+    public WebElement help;
+
+    @FindBy(xpath = "//*[text()=' Products']")
+    public WebElement products;
+
+    @FindBy(xpath = "//p[text()=' Orders']")
+    public WebElement orders;
+
+    @FindBy(xpath = "//p[text()=' Customers']")
+    public WebElement customersText;
+
+    @FindBy(xpath = "//p[text()=' Discounts']")
+    public WebElement discounts;
+
+    @FindBy(xpath = "//p[text()=' Topics (pages)']")
+    public WebElement topicPages;
+
+    @FindBy(xpath = "//p[text()=' Email accounts']")
+    public WebElement emailAccounts;
+
+    @FindBy(xpath = "//p[text()=' Log']")
+    public WebElement log;
+
+    @FindBy(xpath = "//p[text()=' PowerBI']")
+    public WebElement powerBi;
+
+    @FindBy(xpath = "//p[text()=' Training']")
+    public WebElement Training;
 }
+
--- src/Test/_02_CheckLeftNawMenu.java
+++ src/Test/_02_CheckLeftNawMenu.java
@@ -1,4 +1,45 @@
 package Test;
 
-public class _02_CheckLeftNawMenu {
+import Utlity.BaseDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _02_CheckLeftNawMenu extends BaseDriver {
+
+    @Test
+    public void Test(){
+        POM pom=new POM();
+
+        driver.navigate().to("https://admin-demo.nopcommerce.com/login?");
+        pom.logIn.click();
+
+        Assert.assertTrue(pom.logOut.getText().contains("Logout"));
+
+        pom.catalog.click();
+        Assert.assertEquals(pom.products.getText(),"Products");
+
+        pom.sales.click();
+        Assert.assertEquals(pom.orders.getText(),"Orders");
+
+        pom.customers.click();
+        Assert.assertEquals(pom.customersText.getText()," Customers");
+
+        pom.promotions.click();
+        Assert.assertEquals(pom.discounts.getText()," Discounts");
+
+        pom.contentManagement.click();
+        Assert.assertTrue(pom.topicPages.getText().contains("Topics (pages)"));
+
+        pom.configuration.click();
+        Assert.assertTrue(pom.emailAccounts.getText().contains("Email accounts"));
+
+        pom.system.click();
+        Assert.assertTrue(pom.log.getText().contains("Log"));
+
+        pom.reports.click();
+        Assert.assertTrue(pom.powerBi.getText().contains("PowerBI"));
+
+        pom.help.click();
+        Assert.assertTrue(pom.Training.getText().contains("Training"));
+    }
 }
